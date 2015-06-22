e9e32fd5307fb7eccff0065134430c3b3123446f
Parth Shah <parth95shah@users.noreply.github.com>
2015-06-22 23:31:22 +0530

    Updated the enemy and character forms
    Removed the bitmaps and added scalable images

--- PACMAN.C
+++ PACMAN.C
@@ -11,18 +11,10 @@ void enemyMovement();//tracks the enemy AI movement on the screen
 void drawEnemy();//draws the enemy AI on the screen
 void scoreCalc();//tracks the score
 void keyListener();//listens for any key inputs
-int pacman[9][9]={{0,0,1,1,1,1,1,0,0},
-		  {0,1,1,1,1,1,1,1,0},
-		  {1,1,1,1,1,1,1,0,0},
-		  {1,1,1,1,1,1,0,0,0},
-		  {1,1,1,1,1,0,0,0,0},
-		  {1,1,1,1,1,1,0,0,0},
-		  {1,1,1,1,1,1,1,0,0},
-		  {0,1,1,1,1,1,1,1,0},
-		  {0,0,1,1,1,1,1,0,0}};  //pacman bitmap  array
 int inIndex;
 int outIndex;
-
+int maxX;
+int maxY;
 int main()
 {
 	int gdriver = DETECT, gmode, errorcode; //640 * 480 screen
@@ -35,10 +27,11 @@ int main()
 		getch();
 		exit(1);
 	}
-
+	maxX=getmaxx();
+	maxY=getmaxy();
 	drawMap();
 	drawPacman();
-	drawFood();
+	//drawFood();
 	getch();
 	closegraph();
 	return 0;
@@ -46,16 +39,42 @@ int main()
 
 void drawPacman()
 {
-	for(outIndex=0;outIndex<9;outIndex++)
-		for(inIndex=0;inIndex<9;inIndex++)
-			if(pacman[inIndex][outIndex]==1)
-				putpixel(getmaxx()/8+outIndex,getmaxy()/2+inIndex,YELLOW);
+	int ghostLegs[20];
+	ghostLegs[0]=maxX/2-16;
+	ghostLegs[1]=maxY/2+8;
+	ghostLegs[2]=maxX/2-16;
+	ghostLegs[3]=maxY/2+16;
+	ghostLegs[4]=maxX/2-11;
+	ghostLegs[5]=maxY/2+8;
+	ghostLegs[6]=maxX/2-6;
+	ghostLegs[7]=maxY/2+16;
+	ghostLegs[8]=maxX/2;
+	ghostLegs[9]=maxY/2+8;
+	ghostLegs[10]=maxX/2+6;
+	ghostLegs[11]=maxY/2+16;
+	ghostLegs[12]=maxX/2+10;
+	ghostLegs[13]=maxY/2+8;
+	ghostLegs[14]=maxX/2+16;
+	ghostLegs[15]=maxY/2+16;
+	ghostLegs[16]=maxX/2+16;
+	ghostLegs[17]=maxY/2+8;
+	ghostLegs[18]=maxX/2-16;
+	ghostLegs[19]=maxY/2+8;
+	//below 5 lines draw pacman
+	setcolor(YELLOW);
+	setfillstyle(SOLID_FILL,YELLOW);
+	sector(maxX/8,maxY/2,30,330,16,12);
+	setfillstyle(SOLID_FILL,WHITE);
+	setcolor(WHITE);
+	//below lines draw the ghost
+	sector(maxX/2,maxY/2,0,180,16,8);
+	bar(maxX/2-16,maxY/2,maxX/2+16,maxY/2+8);
+	fillpoly(10,ghostLegs);
+	setcolor(RED);
+	setfillstyle(SOLID_FILL,RED);
+	fillellipse(maxX/2-6,maxY/2,4,4);
+	fillellipse(maxX/2+6,maxY/2,4,4);
 }
-
 void drawMap()
 {
-	line(5,5,635,5);
-	line(5,5,5,475);
-	line(5,475,635,475);
-	line(635,475,635,5);
 }
